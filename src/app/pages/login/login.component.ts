4882dae3398adbf6192eb16c043bef9c0d45c727
miilanez <gabrielmilanez99@hotmail.com>
2024-06-28 00:24:52 -0300

    :sparkles: feat: criação da tela de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    const validEmail = 'admin@admin.com';
+    const validPassword = 'password';
+
+
+    //Regras para validação do login
+    if (this.email === validEmail && this.password === validPassword) {
+    //redirecionamento para o dashboard após login
+      this.router.navigate(['/dashboard']);
+    //em caso de falha, mensagem de erro de credenciais
+    } else {
+      this.errorMessage = 'Credenciais de acesso inválidas. Tente novamente.';
+    }
+  }
 }
