318bfa04a560ffb73df36241b4ec97181d9d15c3
miilanez <gabrielmilanez99@hotmail.com>
2024-06-27 23:55:36 -0300

    :heavy_plus_sign: build: adicionando dependências no app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { PainelComponent } from './pages/painel/painel.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { PainelComponent } from './pages/painel/painel.component';
+
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    PainelComponent,
-    CadastroComponent
+    CadastroComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
