11b15979c39df733f3eeb4fd4158037fa3750a1f
miilanez <gabrielmilanez99@hotmail.com>
2024-06-28 16:21:26 -0300

    :construction: feat: leitura dos dados da tela painel

--- src/app/pages/painel/painel.component.ts
+++ src/app/pages/painel/painel.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+interface VirtualMachines {
+  displayName: string;
+  cpu: number;
+  memory: number;
+  status: string;
+}
 
 @Component({
   selector: 'app-painel',
@@ -13,4 +19,12 @@ export class PainelComponent {
   cadastarVM() {
     this.router.navigate(['/cadastro']);
   }
+
+
+  virtualmachines:VirtualMachines[] = [
+    { displayName: 'vm1', cpu: 2, memory: 1024, status: "RUNNING" },
+    { displayName: 'vm2', cpu: 1, memory: 512, status: "PAUSED" },
+    { displayName: 'vm3', cpu: 4, memory: 2048, status: "STOP" }
+  ];
+
 }
