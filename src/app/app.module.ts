cc689fc716ffe9a90be9d2862a91f113a8f0fbfb
miilanez <gabrielmilanez99@hotmail.com>
2024-06-27 23:48:54 -0300

    :sparkles: feat: criação das paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PainelComponent } from './pages/painel/painel.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    PainelComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/painel/painel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrl: './painel.component.css'
+})
+export class PainelComponent {
+
+}
